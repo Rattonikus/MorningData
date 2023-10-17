6c508e54bbcb7807c603b2ffde13edba36ab50ba
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2023-10-17 08:22:08 -0600

    Added gitignore, created proj

---
+++ MorningData/Resources/MorningDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningDataApp.swift
+//  MorningData
+//
+//  Created by Elkins, Cameron on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MorningDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DataView()
+        }
+    }
+}
---
+++ MorningData/Views/DataView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  MorningData
+//
+//  Created by Elkins, Cameron on 10/17/23.
+//
+
+import SwiftUI
+
+struct DataView: View 
+{
+    var body: some View 
+    {
+        VStack 
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview 
+{
+    DataView()
+}
