cbd400a874e6bf08bc3f60d02607dc35ddd7a6e5
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2023-10-17 09:49:24 -0600

    added caritem struct

---
+++ MorningData/Model/CarItem.swift
@@ -0,0 +1,19 @@
+//
+//  CarItem.swift
+//  MorningData
+//
+//  Created by Elkins, Cameron on 10/17/23.
+//
+
+import Foundation
+
+struct CarItem : Codable
+{
+    var EngineType : String
+    var MPG : Int
+    var FuelType : String
+    var Classification : String
+    var Make : String
+    var Model : String
+    var Year : Int
+}
