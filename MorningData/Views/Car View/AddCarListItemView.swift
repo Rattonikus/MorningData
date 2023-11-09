3630ad3fd879185138ac003dd226bc6213d89f11
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2023-11-09 08:58:04 -0700

    added add car view

--- MorningData/Views/Car View/AddCarListItemView.swift
+++ MorningData/Views/Car View/AddCarListItemView.swift
@@ -12,16 +12,43 @@ struct AddCarListItemView: View
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var storedCars : CarItemStore
     @State private var make : String = ""
-    @State private var CarItem : String = ""
+    @State private var carItem : String = ""
     
-    
+    init (with make: String, having item : String)
+    {
+        _make = State(initialValue: make)
+        _carItem = State(initialValue: item)
+    }
     
     var body: some View
     {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form
+        {
+            Section("Make a new car")
+            {
+                InputField(title: "Who made it", hint: "Make", result: $make)
+                InputField(title: "Whats the model", hint: "model", result: $carItem)
+            }
+            if(!make.isEmpty && !carItem.isEmpty)
+            {
+                Button("Add me to list", action: addCarListItemToDataStore)
+            }
+        }
     }
+    private func addCarListItemToDataStore() -> Void
+    {
+        let year = Calendar.current.component(.year, from: Date())
+        
+        let newCarItem = CarItem(EngineType: "", MPG: 23, FuelType: "", Classification: "", Make: make, Model: carItem, Year: year)
+        storedCars.cars.insert(newCarItem, at: 0)
+        dismiss()
+    }
+    
+    
+    
 }
 
-#Preview {
-    AddCarListItemView()
+#Preview ("Add a car")
+{
+    AddCarListItemView(with: "", having: "")
 }
